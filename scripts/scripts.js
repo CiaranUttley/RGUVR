6293ae51a1e9af89946eb8f411c73183e0d7a5dc
CiaranUttley <92799697+CiaranUttley@users.noreply.github.com>
2023-07-19 19:46:46 +0100

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,13 @@
+//java script that displays a message when the submit button is pressed and the
+
 document.getElementById("myForm").addEventListener("submit", function(event) {
     event.preventDefault();
-  
+      
+    
     
     // Display feedback message
     document.getElementById("feedback").textContent = "Form submitted successfully! Please refresh the page.";
   });
 
 
-  
+ 
---
+++ scripts/validate.js
@@ -0,0 +1,12 @@
+
+function submit_event() {
+var email = document.getElementById("myForm")("email").value;
+var date = document.forms["event"]["event_date"].value;
+if (email == "") {
+alert("An event must have a title!");
+}
+else {
+if (alert("Event submitted! The society will contact you soon!"));
+}
+}
+
