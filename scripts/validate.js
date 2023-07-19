023375be5fe97864e385bec146b7402b5cb641e7
CiaranUttley <92799697+CiaranUttley@users.noreply.github.com>
2023-07-19 20:22:41 +0100

    Add files via upload

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,12 +1,16 @@
 
 function submit_event() {
-var email = document.getElementById("myForm")("email").value;
-var date = document.forms["event"]["event_date"].value;
-if (email == "") {
-alert("An event must have a title!");
-}
-else {
-if (alert("Event submitted! The society will contact you soon!"));
-}
-}
-
+    var email = document.forms["myForm"]["Email"].value;
+    var date = document.forms["event"]["event_date"].value;
+    if (email == "") {
+    alert("An event must have a title!");
+    }
+    else {
+    if (date.length == 0) {
+    alert("An event must have a date!");
+    }
+    else {
+    alert("Event submitted! The society will contact you soon!");
+    }
+    }
+    }
