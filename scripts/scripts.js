e9a8a4de0f501280d329f1b694f45f7635ac6506
CiaranUttley <92799697+CiaranUttley@users.noreply.github.com>
2023-07-20 12:28:46 +0100

    Add files via upload

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,19 @@
 //java script that displays a message when the submit button is pressed and the
 
 document.getElementById("myForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-      
-    
-    
-    // Display feedback message
-    document.getElementById("feedback").textContent = "Form submitted successfully! Please refresh the page.";
-  });
+  // Prevent form submission to observe the effect
+  event.preventDefault();
 
+  // Show a confirmation dialog before submitting the form
+  if (confirm("Are you sure you want to submit the form?")) {
+    // Submit the form if user clicks "OK"
+    this.submit();
+    document.getElementById("feedback").textContent = "Form submission succesful";
+    document.getElementById("myForm").innerHTML="Form has been sent successfully! Please refresh the page."
+  } else {
+    // If user clicks "Cancel," do nothing
+    document.getElementById("feedback").textContent = "Form submission canceled.";
+  }
+});
 
  
